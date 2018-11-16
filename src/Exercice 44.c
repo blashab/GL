7ba23417620fc088e8f60555b6f7c6b03b86bd6c
blashab <44798952+blashab@users.noreply.github.com>
2018-11-16 16:44:56 +0100

    Add files via upload

---
+++ src/Exercice 44.c
@@ -0,0 +1,13 @@
+// Devoir GL Lashab Bilâl
+#include "TP_lib.h"  
+
+int main(void)
+{
+    short resultat, i=0; 
+    while(i<10)
+    {
+        resultat=LancerDe();
+        i++;
+    }
+    i=0;
+}
