c6868b87c4cdd6cd69bddfccbea18651abe68994
blashab <44798952+blashab@users.noreply.github.com>
2018-11-16 16:47:02 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,25 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+// Devoir GL Lashab Bilâl   
 
-#include "my_header.h"
+#include "TP_lib.h"  
 
 int main(void)
 {
-    return 0;
+
+    unsigned int Points=0,resultat,perdu=0;
+    
+    while(perdu!=1)
+    {
+        resultat=LancerDe();
+        if(resultat==1)
+            perdu=1;
+		
+        else if(resultat%2==0)
+            Points=Points+resultat;
+		
+        else if(resultat==3)
+            Points=Points*2;
+		
+        else if(resultat==5)
+            Points=Points-2;
+    }
 }
