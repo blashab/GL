4cc804c14a6e5c34986967b3855b0785392060f6
blashab <44798952+blashab@users.noreply.github.com>
2018-11-19 15:08:59 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,47 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
-#include "my_header.h"
+		// Devoir GL Lashab Bilâl   
+#include <ansi_c.h>
 
 int main(void)
 {
-    return 0;
+   int somme1=3,somme2=3,somme3=0,somme4=0,n=2,a=0,rnd=0;
+   
+   
+   while(n<50)
+   {
+       somme1=somme1+3*n;
+       n=n+1;
+   }
+   n=2;
+   
+   while(a<9876)
+   {
+       somme2=somme2+3*n;
+       a=3*n;
+       n=n+1;
+       
+   }
+   
+    while(n<12)
+    {
+        rnd=rand()%10+1;
+        somme3=somme3+rnd;
+        n=n+1;
+    }
+   n=0;
+   
+   if_valides<20
+   {
+       if(n%3==0)
+        {
+            somme4=somme4+n;
+            if_valides = if_valides +1;
+        }
+       else if(n%7==0)
+        {
+            somme4=somme4+n;
+            n=n+1;
+       
+       }
+        n=n+1;
+    }
 }
