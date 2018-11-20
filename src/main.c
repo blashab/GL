124642dbd9ba39fc3f53f207ac188f09d6697ca1
bilal lashab <bilal.lashab@ge1.iut-cachan.u-psud.fr>
2018-11-20 09:49:30 +0100

    Je demarre exo 56

--- src/main.c
+++ src/main.c
@@ -5,5 +5,25 @@
 
 int main(void)
 {
-    return 0;
+	
+unsigned char seuil=6, i;
+unsigned char bin [10]=
+{1,2,3,4,5,6,7,8,9,10};
+
+for (i=0;i<10;i=i+1)
+{
+	
+	if (i<seuil)
+	{
+		bin[10]=0;
+	}
+	else (i>seuil);
+	{
+		bin [10] =1;
+	}
 }
+
+
+}
+
+
