769ba5714dd8bfa421abd7ea9c66ebf38b8a1bb8
bilal lashab <bilal.lashab@ge1.iut-cachan.u-psud.fr>
2018-12-04 09:50:44 +0100

    Je capture uniquement certaines variables, je commence exo 79

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <ansi_c.h>
 #include "my_header.h"
 
 int main(void)
 {
+	char phrase[100]= "je m'appelle Bilal Lashab et j'ai 18 ans";
+	char prenom [100];
+	char nom [100];
+    unsigned int age;
+	
+	
+    scanf("%s %s %u", prenom , nom , &age);
+
     return 0;
 }
