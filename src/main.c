97079e1fd8eb06c7d097f9af7466143a090bf8e3
blashab <44798952+blashab@users.noreply.github.com>
2018-12-10 22:50:19 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,18 +1,27 @@
 //  Copyright (c) 2018 Antoine Tran Tan
-//
+// 
+// Exo79 Lashab Bilâl
 
 #include <ansi_c.h>
 #include "my_header.h"
+#include <userint.h>
 
 int main(void)
 {
-	char phrase[100]= "je m'appelle Bilal Lashab et j'ai 18 ans";
-	char prenom [100];
-	char nom [100];
-    unsigned int age;
-	
-	
-    scanf("%s %s %u", prenom , nom , &age);
+	char prenom[100];
+	char nom[100];
+	int age;
+	int anneeNaissance;
+	char retour[100];
+
+scanf("%s",prenom);
+scanf("%s",nom);
+scanf("%d",&anneeNaissance);
+
+age = 2018 - anneeNaissance;
+
+sprintf(retour,"Je m'appelle %s %s et j'ai %d ans" ,prenom,nom,age);
+MessagePopup("retour",retour);
 
     return 0;
 }
